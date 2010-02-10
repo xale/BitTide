a36c3d57a7035954d012bc6060d05d9ded425bef
Alex Rozenshteyn <rpglover@eridani.acm.jhu.edu>
2010-02-10 16:13:08 -0500

    Added enum LogState and preliminary DB
    LogState is an enum of the three possible login states.
    Database is a wrapper that allows you to lookup user ids or files.

---
+++ tracker/Database.java
@@ -0,0 +1,48 @@
+package tracker;
+
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
+import tracker.UserRecord;
+public class Database
+{
+	private Map<Integer, UserRecord> userDB;
+	private Map<String, Set<Integer>> fileDB;
+	/**
+	  * @return null if the fileName is not in the database,
+	  * a set of user ids otherwise.
+	  * @param fileName the name of the file to look up.
+	  */
+	public Set<Integer> getUserIDsFromFileName(String fileName)
+	{
+		return fileDB.get(fileName);
+	}
+	/**
+	  * @return null if the fileName is not in the database,
+	  * a set of user records otherwise.
+	  * @param fileName the name of the file to look up.
+	  */
+	public Set<UserRecord> getUsersFromFileName(String fileName)
+	{
+		Set<Integer> userIDs = getUserIDsFromFileName(fileName);
+		if (userIDs == null)
+		{
+			return null;
+		}
+		Set<UserRecord> userRecords = new HashSet();
+		for (Integer uid : userIDs)
+		{
+			userRecords.add(userDB.get(uid));
+		}
+		return userRecords;
+	}
+	/**
+	  * @return null if the user id doesn't exist, the record otherwise.
+	  * @param uid the integer id of the user.
+	  */
+	public UserRecord getUserRecordFromID(int uid)
+	{
+		return userDB.get(uid);
+	}
+}
