858a01a4b81b0eb7e66b93d191685176841faca8
Alex Heinz <xale@acm.jhu.edu>
2010-02-17 22:20:01 -0500

    Wrote the raw-byte constructor for the SearchReplyMessage class.

--- message/SearchReplyMessage.java
+++ message/SearchReplyMessage.java
@@ -1,13 +1,64 @@
 package message;
 
+import java.net.*;
 import java.nio.*;
+import java.util.*;
 
 public class SearchReplyMessage extends Message
 {
-
+	private static final int SIZE_FIELD_WIDTH =		Integer.SIZE;	// Four bytes
+	
+	private static final int IP_FIELD_WIDTH =		Integer.SIZE;	// Four bytes
+	private static final int PORT_FIELD_WIDTH =		Short.SIZE;		// Two bytes
+	private static final int BITMAP_FIELD_WIDTH = 	12;
+	private static final int PEER_ENTRY_WIDTH = IP_FIELD_WIDTH + PORT_FIELD_WIDTH + BITMAP_FIELD_WIDTH;
+	
+	private long fileSize;
+	private SearchReplyPeerEntry[] peerResults;
+	
 public SearchReplyMessage(ByteBuffer contents)
 {
-	// FIXME: WRITEME
+	// Retrieve the size of the file
+	fileSize = (contents.getInt() & 0xFFFFFFFFL);
+	
+	// Determine the number of peers returned in the search result
+	int numPeers = ((contents.array().length - SIZE_FIELD_WIDTH) / PEER_ENTRY_WIDTH);
+	
+	// Parse the entries
+	peerResults = new SearchReplyPeerEntry[numPeers];
+	for (int i = 0; i < numPeers; i++)
+	{
+		try
+		{
+			// Read the peer's ip address
+			byte[] ip = new byte[IP_FIELD_WIDTH];
+			contents.get(ip);
+			
+			// Read the peer's listening port
+			int port = (contents.getShort() & 0x0000FFFF);
+			
+			// Read the peer's bitmap of the file
+			byte[] fileBitmap = new byte[BITMAP_FIELD_WIDTH];
+			contents.get(fileBitmap);
+			
+			// Create the entry
+			peerResults[i] = new SearchReplyPeerEntry(ip, port, fileBitmap);
+		}
+		catch (UnknownHostException UHE)
+		{
+			peerResults[i] = null;
+		}
+	}
+}
+
+public long getFileSize()
+{
+	return fileSize;
+}
+
+public SearchReplyPeerEntry[] getPeerResults()
+{
+	return peerResults;
 }
 
 public MessageCode getMessageCode()
