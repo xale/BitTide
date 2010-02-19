252a2bfa04744f074a99f7c22a3fffaa5ee10215
Alex Heinz <xale@acm.jhu.edu>
2010-02-19 09:49:04 -0500

    Split the HEADER_LENGTH constant into a CODE_FIELD_WIDTH and a LENGTH_FIELD_WIDTH, representing the size of the parts of the message header. Also added an abstract accessor for retrieving the full length (in bytes) of the raw message.

--- message/Message.java
+++ message/Message.java
@@ -4,10 +4,14 @@ import java.nio.*;
 
 public abstract class Message
 {
-	public static final int HEADER_LENGTH = 5;
+	public static final int CODE_FIELD_WIDTH = (Byte.SIZE / Byte.SIZE);
+	public static final int LENGTH_FIELD_WIDTH = (Integer.SIZE / Byte.SIZE);
+	public static final int HEADER_LENGTH = CODE_FIELD_WIDTH + LENGTH_FIELD_WIDTH;
 	public static final long MAX_MESSAGE_LENGTH = ((16 * 1024) + 2) + HEADER_LENGTH;
 
 public abstract MessageCode getMessageCode();
+
+public abstract long getRawMessageLength();
 
 public abstract ByteBuffer getRawMessage();
 
