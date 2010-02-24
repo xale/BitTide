c72cd54dc6d9ab48b0431c1348a328191627d9dd
Alex Heinz <xale@acm.jhu.edu>
2010-02-24 10:32:40 -0500

    Renamed the PeerToPeerConnectionHandler class to IncomingPeerConnection, to shorten the name and better differentiate it from outgoing connections.

---
+++ peer/IncomingPeerConnection.java
@@ -0,0 +1,73 @@
+package peer;
+
+import java.net.*;
+import java.util.*;
+import java.io.*;
+
+public class IncomingPeerConnection implements Runnable
+{
+	private Socket peerSocket = null;
+	
+public IncomingPeerConnection(Socket connectionSocket)
+{
+	peerSocket = connectionSocket;
+}
+
+public void run()
+{
+	InputStream readStream;
+	OutputStream writeStream;
+	
+	try
+	{
+		// Open streams to peer
+		readStream = peerSocket.getInputStream();
+		writeStream = peerSocket.getOutputStream();
+	}
+	catch (Exception E)
+	{
+		// Failed to open streams; abort
+		return;
+	}
+
+	try
+	{
+		// FIXME: WRITEME: handle connection
+	}
+	catch (Exception e)
+	{
+		e.printStackTrace();
+	}
+	
+	try
+	{
+		// Close write stream
+		writeStream.close();
+	}
+	catch (IOException IOE)
+	{
+		// Do nothing...
+	}
+	
+	try
+	{
+		// Close read stream
+		readStream.close();
+	}
+	catch (IOException IOE)
+	{
+		// Do nothing...
+	}
+	
+	try
+	{
+		// Close socket
+		peerSocket.close();
+	}
+	catch (IOException IOE)
+	{
+		// Do nothing...
+	}
+}
+
+}
