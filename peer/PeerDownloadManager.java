892cca506c00d34308ca458f9bdaf5ec34ff889f
Alex Heinz <xale@acm.jhu.edu>
2010-02-24 23:33:22 -0500

    Lots of work on the DownloadMananger: - Added a LinkedList to keep track of filenames in the order their downloads were started, so that they can be printed to the user in that order - Changed the stopDownloads method to just stop the downloads, rather than stopping downloads and shutting down the pool of worker threads. - Added a shutdown method that does what stopDownloads did previously (i.e., stops downloads and shuts down the thread pool.) - Removed the accessors for getting and setting the status of a download by its filename; the download manager will now handle this itself. - Renamed the updateDownloadBitmap method to blockReceived, changed its arguments to take a block index number instead of a bitmap (since we're only ever updating on block at a time) and added a call to push the updated bitmap to the tracker. - Added a downloadFailed accessor, called by worker threads when their download fails for any reason. - Wrote implementations of the printDownloadStatusList and getNumDownloadsInProgress methods.

--- peer/PeerDownloadManager.java
+++ peer/PeerDownloadManager.java
@@ -9,6 +9,7 @@ public class PeerDownloadManager
 {
 	private File downloadsDirectory = null;
 	private Map<String, PeerDownloadFile> downloads = null;
+	private List<String> downloadList = null;
 	
 	private PeerToTrackerConnection trackerConnection = null;
 	
@@ -22,6 +23,9 @@ public PeerDownloadManager(PeerToTrackerConnection tracker, File downloadDir)
 	// Create a map of filenames to download information objects
 	downloads = new HashMap<String, PeerDownloadFile>();
 	
+	// Create a list of filenames, maintaining the order in which they were downloaded
+	downloadList = new LinkedList<String>();
+	
 	// Create a pool of worker threads
 	threadPool = Executors.newCachedThreadPool();
 }
@@ -33,12 +37,29 @@ public synchronized void startDownload(String filename, SearchReplyMessage downl
 
 public synchronized void stopDownloads()
 {
+	// Iterate through the list of downloads
+	for (Map.Entry<String, PeerDownloadFile> file : downloads.entrySet())
+	{
+		// If this is not a download in-progress, ignore it
+		if (file.getValue().getDownloadStatus().isStopped())
+			continue;
+		
+		// Cancel downloads in progress
+		file.getValue().setDownloadStatus(PeerDownloadStatus.canceled);
+	}
+}
+
+public synchronized void shutdown()
+{
 	// Stop accepting new tasks to the thread pool
 	threadPool.shutdown();
 	
+	// Cancel all downloads in-progress
+	this.stopDownloads();
+	
 	try
 	{
-		// If the current tasks don't finish in ten seconds, force termination
+		// If the thread pool's tasks don't finish in ten seconds, force termination
 		if (!threadPool.awaitTermination(10, TimeUnit.SECONDS))
 			threadPool.shutdownNow();
 	}
@@ -49,46 +70,82 @@ public synchronized void stopDownloads()
 	}
 }
 
-public synchronized void updateDownloadStatus(String filename, PeerDownloadStatus status)
+public synchronized boolean blockReceived(String filename, int blockIndex)
 {
 	// Locate the file to update
 	PeerDownloadFile file = downloads.get(filename);
 	
-	// If the file is in a "stopped" state, (i.e., finished downloading, failed, or canceled) do not change its status
+	// Check if the file is still being downloaded
 	if (file.getDownloadStatus().isStopped())
-		return;
-	
-	// Otherwise, update the status
-	file.setDownloadStatus(status);
-}
-
-public synchronized PeerDownloadStatus getDownloadStatus(String filename)
-{
-	return downloads.get(filename).getDownloadStatus();
-}
-
-public synchronized void updateDownloadBitmap(String filename, FileBitmap bitmap)
-{
-	// Locate the file to update
-	PeerDownloadFile file = downloads.get(filename);
+		return false;
 	
 	// Update the bitmap
-	file.updateReceivedBitmap(bitmap);
+	file.updateReceivedBitmap(blockIndex);
+	
+	// Send the updated bitmap to the tracker
+	try
+	{
+		trackerConnection.sendMessage(new FileBitmapMessage(filename, file.getReceivedBitmap()));
+	}
+	catch (Exception E)
+	{
+		
+		return false;
+	}
 	
 	// If necessary, update the download status
 	if (file.getDownloadStatus() == PeerDownloadStatus.notStarted)
 		file.setDownloadStatus(PeerDownloadStatus.inProgress);
+	
+	return true;
+}
+
+public synchronized void downloadFailed(String filename)
+{
+	// Locate the file in question
+	PeerDownloadFile file = downloads.get(filename);
+	
+	// If the download has not already been stopped for other reasons, update the status
+	if (!file.getDownloadStatus().isStopped())
+		file.setDownloadStatus(PeerDownloadStatus.failed);
 }
 
 public synchronized void printDownloadStatusList()
 {
-	// FIXME: WRITEME
+	// Iterate through the list of downloads, in the order they were added
+	PeerDownloadFile file;
+	for (String filename : downloadList)
+	{
+		// Get the download entry
+		file = downloads.get(filename);
+		
+		// Print the name and status of the file
+		System.out.println(filename + " (" + file.getDownloadStatus() + "):");
+		
+		// Calculate the download progress
+		int downloadedBlocks = file.getReceivedBitmap().getNumberOfBlocks();
+		int totalBlocks = (int)Math.ceil((double)file.getFileSize() / (double)Message.MAX_BLOCK_SIZE);
+		
+		// FIXME: calculate bytes downloaded out of total bytes?
+		
+		// Print the download progress
+		System.out.println("\t" + downloadedBlocks + " out of " + totalBlocks + " blocks");
+	}
 }
 
 public synchronized int getNumDownloadsInProgress()
 {
-	// FIXME: WRITEME
-	return 0;
+	int countInProgress = 0;
+	
+	// Iterate through the list of downloads
+	for (Map.Entry<String, PeerDownloadFile> file : downloads.entrySet())
+	{
+		// If this is a download in-progress, increment the count
+		if (!file.getValue().getDownloadStatus().isStopped())
+			countInProgress++;
+	}
+	
+	return countInProgress;
 }
 
 }
